ab0359cf196ce1b79c84e65ea36a3b1242d3adfa
Angela Clarke <angeladoust@hotmail.com>
2022-08-09 09:17:41 +1000

    todo completed

--- src/App.js
+++ src/App.js
@@ -4,15 +4,45 @@ import TodoItem from "./components/TodoItem";
 import "./App.css";
 
 function App() {
-const [todos, setTodos] = useState([
-  "learn about react",
-  "Meet friend for lunch",
-  "Build really cool todo app",
+const [todos, setTodos] = useState([ //an array of objects
+  { //object, dead give away is key value objects eg text, isCompleted and value: False
+  text: "learn about react",
+  isCompleted: false,
+  },
+  {
+  text: "Meet friend for lunch",
+  isCompleted: false,
+  },
+  {
+  text: "Build really cool todo app",
+  isCompleted: false,
+  },
 ]);
 
 const addTodo = (text) => {
-  const newTodos = [...todos, text];
+  const newTodos = [...todos, { text }];
   setTodos(newTodos);
+};
+
+const completeTodo = (index) => {
+  const newTodos = [...todos];
+  newTodos[index].isCompleted = true;
+  setTodos(newTodos);
+};
+
+const removeTodo = (index) => {
+  const newTodos = [...todos];
+  newTodos.splice(index,1);
+  setTodos(newTodos);
+};
+
+
+// setting a variable of returnTodo as an outcome of an anonymous function
+// index refers to the todo
+const returnTodo = (index) => {
+  const newTodos = [...todos]; //spread operator allows us to 'copy and paste and add 1'
+  newTodos[index].isCompleted = false; //editing an atribute of one particular item and making it false
+  setTodos(newTodos); //updating state
 };
 
   return (
@@ -20,7 +50,9 @@ const addTodo = (text) => {
       <div className="todo-list">
   <h1>My todo list</h1>
   {todos.map((todo, index) => ( 
-    <TodoItem todo={todo} key={index} />
+    <TodoItem todo={todo} key={index} index={index} completeTodo={completeTodo} 
+    removeTodo ={removeTodo}
+    returnTodo ={returnTodo} /> //attributes on the element
   ))}
   <TodoForm addTodo={addTodo}/>
   </div>
--- src/components/TodoItem/index.jsx
+++ src/components/TodoItem/index.jsx
@@ -2,8 +2,17 @@ import React from "react";
 import "./TodoItem.css"
 
 function TodoItem(props) {
-    const { todo } = props;
-    return <div className="todo">{todo}</div>;
+    // destructruring through the props, giving us access to use these functions on the props https://medium.com/@lcriswell/destructuring-props-in-react-b1c295005ce0
+    const { todo, index, completeTodo, removeTodo, returnTodo } = props;
+    return <div className={`${todo.isCompleted ? "complete": ""}`}>
+        {todo.text}
+        <div>
+            <button onClick={()=> completeTodo(index)}>Complete</button>
+            <button onClick={()=> removeTodo(index)}>x</button>
+            <button onClick={()=> returnTodo(index)}>Oops</button> 
+            {/* passing index into returnTodo */}
+        </div>
+    </div>;
 }
 
 export default TodoItem;
